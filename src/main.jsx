985ca7d19b62c74bf0a3ee332ed5f3346d78ab26
Lilmun02 <jamarioushumphries0822@gmail.com>
2026-08-23 23:45:31 -0400

    Load live PWA camera styles

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles.css'
 import './gifts.css'
+import './live.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
