03aeff3e67ecb8c37f076c95b582137822dcfa8c
Lilmun02 <jamarioushumphries0822@gmail.com>
2026-08-24 00:40:06 -0400

    Load Fameverse account foundation styles

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './styles.css'
 import './gifts.css'
 import './live.css'
+import './foundation.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
