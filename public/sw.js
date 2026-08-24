093b5522b710cb9dff760ec6710366deaee69bca
Lilmun02 <jamarioushumphries0822@gmail.com>
2026-08-24 16:58:37 -0400

    chore: refresh PWA cache for touch-selection fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fameverse-beta-v8'
+const CACHE = 'fameverse-beta-v9'
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icon.svg']
 
 self.addEventListener('install', (event) => {
