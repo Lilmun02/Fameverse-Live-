96375774a561fb76a1e09b403eb846d0785acdfe
Lilmun02 <jamarioushumphries0822@gmail.com>
2026-08-24 22:33:54 -0400

    fix: FAM-9 bound auth startup and refresh PWA cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fameverse-beta-v11'
+const CACHE = 'fameverse-beta-v12'
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icon.svg']
 
 self.addEventListener('install', (event) => {
