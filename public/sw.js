9ca38cfe2a01182fbda6eca389a111f76c66146d
Lilmun02 <jamarioushumphries0822@gmail.com>
2026-08-26 17:24:45 -0400

    chore: refresh PWA cache for FAM-8 media fix

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fameverse-beta-v13'
+const CACHE = 'fameverse-beta-v14'
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icon.svg']
 
 self.addEventListener('install', (event) => {
