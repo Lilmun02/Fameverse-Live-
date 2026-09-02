2b85e29ea2b4531a51e8a91bb1c216ff08ff962d
Lilmun02 <jamarioushumphries0822@gmail.com>
2026-09-02 15:51:34 -0400

    fix: expose persistent gift audio session controls

--- src/features/gifts/renderer/gift-engine.js
+++ src/features/gifts/renderer/gift-engine.js
@@ -1,5 +1,10 @@
 import '../../../styles/gifts/engine.css'
-import { playGiftChime, primeGiftAudio } from './gift-audio.js'
+import {
+  playGiftChime,
+  primeGiftAudio,
+  startGiftAudioSession,
+  stopGiftAudioSession,
+} from './gift-audio.js'
 
 const GROK_WELCOME_VIDEO = 'https://d2ol7oe51mr4n9.cloudfront.net/user_3IL6AXXAqcrsLZJmbjvrquIP0Bd/8d3fd7e2-9073-4e1b-8ef6-843a1514aae6.mp4'
 
@@ -156,6 +161,8 @@ document.addEventListener('fameverse:gift', (event) => {
 window.FameverseGiftEngine = Object.freeze({
   play: playGift,
   primeAudio: primeGiftAudio,
+  startAudioSession: startGiftAudioSession,
+  stopAudioSession: stopGiftAudioSession,
   registry: giftRegistry,
   stop: stopGiftEngine,
 })
